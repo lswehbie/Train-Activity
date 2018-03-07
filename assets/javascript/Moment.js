bc0a04635115b4e2255262c6df804ebe6768a777
Wehbiels <Lara.Wehbie@gmail.com>
2018-03-07 11:13:30 -0500

    Train-Activity

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -29,47 +29,62 @@ var frequency;
 // var minutesAway;
 $("#submit").on("click", function() {
 	event.preventDefault();
-	trainName = $("#trainName-input")
-		.val()
-		.trim();
-	destination = $("#destination-input")
-		.val()
-		.trim();
-	firstTrainTime = $("#firstTrainTime-input")
-		.val()
-		.trim();
-	frequency = $("#frequency-input")
-		.val()
-		.trim();
-	// nextArrival = $("#nextArrival").val().trim();
-	// minutesAway = $("#minutesAway").val().trim();
-	// Clears the form after pressing submit
+
+	trainName = $("#trainName-input").val().trim();
+	destination = $("#destination-input").val().trim();
+	firstTrainTime = $("#firstTrainTime-input").val().trim();
+	frequency = $("#frequency-input").val().trim();
+
+	
+	// Clears the form after user presses the submit button
+
 	$("#trainName-input").val("");
 	$("#destination-input").val("");
 	$("#firstTrainTime-input").val("");
 	$("#frequency-input").val("");
+
 	database.ref().push({
 		trainName: trainName,
 		destination: destination,
-		firstTrainTime: firstTrainTime,
+		//firstTrainTime: firstTrainTime,
 		frequency: frequency,
 		TIMESTAMP: firebase.database.ServerValue.TIMESTAMP
 	});
 });
-database.ref().on(
-	"child_added",
-	function(snapshot) {
+
+database.ref().on("child_added",function(snapshot) {
 		var snap = snapshot.val();
 		console.log(snap.trainName);
 		console.log(snap.destination);
 		console.log(firstTrainTime);
 		console.log(snap.frequency);
-		// console.log(nextArrival);
-		// console.log(snap.minutesAway);
-		$("#trainName-display").text(snap.trainName);
-		$("#destination-display").text(snap.destination);
-		$("#firstTrainTime-display").text(snap.firstTrainTime);
-		$("#frequency-display").text(snap.frequency);
+		
+
+		$("#trainName-input").text(snap.trainName);
+		$("#destination-input").text(snap.destination);
+		$("#firstTrainTime-input").text(snap.firstTrainTime);
+		$("#frequency-input").text(snap.frequency);
+
+		console.log("FIRST TRAIN: " + firstTrainTime);
+        //CONVERTS AND CALCULATES TIME FIRST ARRIVAL...
+        //...AND MINUTES AWAY
+        var timeConverted = moment(firstTrainTime, "HH:mm");
+        console.log("TIME CONVERTED: " + timeConverted);
+
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+        var timeDifference = moment().diff(moment(timeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        var remainder = timeDifference % frequency;
+        console.log("REMAINDER: " + remainder);
+
+        var minutesPast = (frequency - remainder);
+        console.log("MINUTES UNTILL NEXT TRAIN: " + minutesTillTrain);
+
+        var nextTrain = moment().add(minutesPast, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
 		var tBody = $("#dataTable");
 		var tRow = $("<tr>");
